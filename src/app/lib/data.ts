175bed802efe7d1d115f274ca587e5d4132639b3
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-11-25 10:24:35 +0100

    hecho la estructura del proyecto

---
+++ src/app/lib/data.ts
@@ -0,0 +1,20 @@
+// Simulo el fetching de datos como si fuese de una api
+const dataFacebook = {
+  followers: 3242,
+  views: 20,
+  likes: 31,
+};
+
+const dataInstagram = {
+  followers: 1565,
+  views: 516,
+  likes: 156,
+};
+
+const dataTwitter = {
+  followers: 1651,
+  retweets: 20,
+  likes: 31,
+};
+
+export { dataFacebook, dataInstagram, dataTwitter };

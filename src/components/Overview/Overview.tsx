a73e7a39b749e6127806247a9f373e176402f74b
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-11-26 18:05:57 +0100

    terminada la estrucutra

--- src/components/Overview/Overview.tsx
+++ src/components/Overview/Overview.tsx
@@ -1,40 +1,59 @@
 import React from "react";
+import IconFacebook from "../Icons/IconFacebook";
+import IconUp from "../Icons/IconUp";
+import IconDown from "../Icons/IconDown";
+import IconTwitter from "../Icons/IconTwitter";
+import IconInstagram from "../Icons/IconInstagram";
 
 export default function Overview() {
   // Overview (Hay cartas diferentes, por lo tanto se divide en más componentes pero tienen el mismo estilo.
   // Algunas llaman a las views y otros a los likes)
   return (
     <section>
-      <article>
-        <div>
-          <p>Page Views</p>
-          <p>Media logo</p>
+      <h2 className="mb-4 text-2xl text-gray-500">Overview - Today</h2>
+
+      <article className="mb-4 flex flex-col gap-y-6 bg-gray-200 py-4">
+        <div className="flex items-center justify-around">
+          <p className="text-lg text-gray-500">Page Views</p>
+          <IconFacebook />
         </div>
-        <div>
-          <p>87</p>
-          <p>3%</p>
+
+        <div className="flex items-center justify-around">
+          <p className="text-5xl font-bold">87</p>
+          <div className="flex items-center">
+            <IconUp />
+            <p className="text-green-500">3%</p>
+          </div>
         </div>
       </article>
 
-      <article>
-        <div>
-          <p>Page Views</p>
-          <p>Media logo</p>
+      <article className="mb-4 flex flex-col gap-y-6 bg-gray-200 py-4">
+        <div className="flex items-center justify-around">
+          <p className="text-lg text-gray-500">Page Views</p>
+          <IconTwitter />
         </div>
-        <div>
-          <p>87</p>
-          <p>3%</p>
+
+        <div className="flex items-center justify-around">
+          <p className="text-5xl font-bold">435</p>
+          <div className="flex items-center">
+            <IconUp />
+            <p className="text-green-500">12%</p>
+          </div>
         </div>
       </article>
 
-      <article>
-        <div>
-          <p>Page Views</p>
-          <p>Media logo</p>
+      <article className="mb-4 flex flex-col gap-y-6 bg-gray-200 py-4">
+        <div className="flex items-center justify-around">
+          <p className="text-lg text-gray-500">Page Views</p>
+          <IconInstagram />
         </div>
-        <div>
-          <p>87</p>
-          <p>3%</p>
+
+        <div className="flex items-center justify-around">
+          <p className="text-5xl font-bold">23</p>
+          <div className="flex items-center">
+            <IconDown />
+            <p className="text-red-500">15%</p>
+          </div>
         </div>
       </article>
     </section>
